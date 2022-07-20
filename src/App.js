7a94d9eedccb4842fc7c01d72465b8f2a81b146a
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-07-20 08:08:27 +0800

    Deploy to github

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+import logo from "./logo.png";
+import Cards from "./components/Cards";
+import Chart from "./components/Chart";
+import CountryPicker from "./components/CountryPicker";
+
+function App() {
+  const [country, setCountry] = useState("");
+  const handleCountryChange = async (e) => {
+    await setCountry(e);
+  };
+
+  return (
+    <div className='container my-4'>
+      <div className='text-center'>
+        <img
+          src={logo}
+          style={{ width: "100px", height: "auto", paddingBottom: "20px" }}
+          alt=''
+        />{" "}
+        <span style={{ fontSize: "30px", color: "#58c5f8" }}>Tracker</span>
+      </div>
+      <Cards country={country} />
+
+      <CountryPicker handleCountryChange={handleCountryChange} />
+
+      <Chart country={country} />
+
+      <div className='card-footer my-5 text-muted text-center'>
+        Developer Contact:{" "}
+        <a href='mailto:nicedre4m@yahoo.com'>nicedre4m@yahoo.com</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
